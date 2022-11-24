8df887b6b504528dcff727660a89bf620370df98
Rindan Maulana <115127635+ZxGvy@users.noreply.github.com>
2022-11-24 09:32:37 +0530

    Create Variabel.java

---
+++ Variabel.java
@@ -0,0 +1,12 @@
+public class Variabel {
+    public static void main(String args[]) {
+      String salam = "Assalamualaikum wr wb.";
+      String nama = "Rindan Maulana Aidil putra";
+      int tl = 2004;
+      
+        
+      System.out.println(salam);
+      System.out.println("Nama saya : "+nama);
+      System.out.println("Saya lahir di tahun : "+tl);
+    }
+}
